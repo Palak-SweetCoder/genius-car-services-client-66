b913a016d379210501e35c097006de0bd543df22
Palak-SweetCoder <pspalak6@gmail.com>
2022-07-03 21:53:24 +0600

    services component problem solved

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -4,6 +4,7 @@ const About = () => {
     return (
         <div>
             <h2>This is about us</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error optio dolor labore dolorum, eveniet corrupti perspiciatis aperiam exercitationem velit minima aut vero debitis reprehenderit sapiente sit voluptatum ratione repellat deleniti. <br /><br /> Ex sunt tempora, quos iste eos placeat! Facilis temporibus sequi iusto quidem ipsam voluptatem officia adipisci accusantium, excepturi corporis. A praesentium temporibus veritatis. Minus autem dolor voluptate fugit, perspiciatis enim, excepturi dolores explicabo doloremque, delectus cumque labore dolorem? Aliquam officia commodi recusandae, magnam dignissimos exercitationem quo ab adipisci facere dolorem. Voluptatibus cumque pariatur illum animi explicabo quidem eveniet maxime, inventore libero a maiores ex nemo. <br /><br /> Similique commodi corrupti a sapiente rem vitae, deserunt cupiditate ex voluptatibus iure fuga culpa. Voluptatibus, asperiores atque? Earum, magnam tempora sunt ut et sequi delectus dolorem nulla dolores dolore laboriosam? Laudantium consequuntur accusamus, praesentium ex maxime porro provident eius voluptas temporibus veniam minima non ab quibusdam iusto excepturi dolorum doloremque nesciunt accusantium sunt rerum perferendis!</p>
         </div>
     );
 };
