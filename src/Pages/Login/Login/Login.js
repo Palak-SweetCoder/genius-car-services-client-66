cad88c5a9b120c724d56265ca0edd2db8524a538
Palak-SweetCoder <pspalak6@gmail.com>
2022-10-26 16:57:19 +0600

    jwt done

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -8,6 +8,7 @@ import SocialLogin from '../SocialLogin/SocialLogin';
 import './Login.css'
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
 
 const Login = () => {
     const emailRef = useRef('');
@@ -32,7 +33,7 @@ const Login = () => {
     const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
 
     if (user) {
-        navigate(from, { replace: true });
+        // navigate(from, { replace: true });
     }
 
     if (loading || sending) {
@@ -48,11 +49,15 @@ const Login = () => {
         errorElement = <p className='text-danger'>Error: {error?.message}</p>
     }
 
-    const handleSubmit = event => {
+    const handleSubmit = async event => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
-        signInWithEmailAndPassword(email, password);
+        await signInWithEmailAndPassword(email, password);
+        //jwt
+        const { data } = await axios.post('http://localhost:5000/login', { email });
+        localStorage.setItem('accessToken', data.accessToken);
+        navigate(from, { replace: true });
     }
 
     const resetPassword = async () => {
--- src/Pages/Order/Order.js
+++ src/Pages/Order/Order.js
@@ -1,13 +1,16 @@
-import axios from 'axios';
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import axiosPrivate from '../../api/axiosPrivate';
 import auth from '../../firebase.init';
 
 const Order = () => {
     const [user] = useAuthState(auth);
     const [orders, setOrders] = useState([]);
+    const navigate = useNavigate();
 
     //Don't be afraid :) every time we used fetch and general arrow function to fetch url
     //Here is a little different and its interesting also short code.
@@ -16,13 +19,22 @@ const Order = () => {
         const getOrders = async () => {
             const email = user.email;
             const url = `http://localhost:5000/order?email=${email}`;
-            const { data } = await axios.get(url);
-            setOrders(data)
+            try {
+                const { data } = await axiosPrivate.get(url);
+                setOrders(data)
+            }
+            catch (error) {
+                console.log(error.message)
+                if (error.response.status === 401 || error.response.status === 403) {
+                    signOut(auth);
+                    navigate('/login')
+                }
+            }
         }
         //Mind it... it's a function we declared so we have to call this function otherwise it will not work
         getOrders();
+    }, [user, navigate])
 
-    }, [user])
     return (
         <div>
             <h1>Your Total Orders: {orders.length}</h1>
